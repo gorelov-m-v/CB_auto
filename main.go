282c4c36915d2c1c8bc048d7e88cb275c4e6d117
mgorelov <gorelov.m@it-webdevelopment.com>
2025-01-15 09:08:07 +0300

    Переход на библиотеку компании для общения с БД

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+import (
+	"fmt"
+	"strconv"
+)
+
+func maximumSalary(numStr string) string {
+	if len(numStr) == 1 {
+		return "0"
+	}
+
+	for i := 0; i < len(numStr)-1; i++ {
+		// Преобразуем символы в числа для сравнения
+		currDigit, _ := strconv.Atoi(string(numStr[i]))   // Текущая цифра
+		nextDigit, _ := strconv.Atoi(string(numStr[i+1])) // Следующая цифра
+
+		if currDigit > nextDigit {
+			continue
+		}
+
+		if currDigit < nextDigit {
+			// Удаляем цифру с индексом i
+			newStr := numStr[:i] + numStr[i+1:]
+			return newStr
+		}
+	}
+
+	// Если ничего не удалили, удаляем последнюю цифру
+	return numStr[:len(numStr)-1]
+}
 
 func main() {
+	var testCount int
+	fmt.Scan(&testCount)
+
+	for i := 0; i < testCount; i++ {
+		var salaryStr string
+		fmt.Scan(&salaryStr)
+		result := maximumSalary(salaryStr)
+		fmt.Println(result)
+	}
 }
--- test/transport/database/database.go
+++ test/transport/database/database.go
@@ -1,39 +1,94 @@
 package database
 
 import (
-	"CB_auto/test/config"
+	"context"
 	"database/sql"
 	"fmt"
 	"time"
+
+	"github.com/jmoiron/sqlx"
+	"gitlab.b2bdev.pro/backend/go-packages/log"
 )
 
-const (
-	dialect = "mysql"
-)
+type Config struct {
+	DriverName string
+	DSN        string
 
-func InitDB(cfg *config.MySQLConfig) (*sql.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
-	db, err := sql.Open(dialect, dsn)
-	if err != nil {
-		return nil, fmt.Errorf("open mysql connection: %w", err)
-	}
-
-	connMaxLifetime, err := time.ParseDuration(cfg.ConnMaxLifetime)
-	if err != nil {
-		return nil, fmt.Errorf("parse connMaxLifetime: %w", err)
-	}
-
-	db.SetConnMaxLifetime(connMaxLifetime)
-	db.SetMaxOpenConns(cfg.MaxOpenConns)
-	db.SetMaxIdleConns(cfg.MaxIdleConns)
-
-	return db, nil
+	PingTimeout     time.Duration
+	ConnMaxLifetime time.Duration
+	ConnMaxIdleTime time.Duration
+	MaxOpenConns    int
+	MaxIdleConns    int
 }
 
-func CloseDB(db *sql.DB) error {
-	if db != nil {
-		return db.Close()
+type Connector struct {
+	db *sqlx.DB
+}
+
+func NewConnector(db *sqlx.DB) Connector {
+	return Connector{db: db}
+}
+
+func OpenConnector(ctx context.Context, config Config) (Connector, error) {
+	db, err := sqlx.Open(config.DriverName, config.DSN)
+	if err != nil {
+		return Connector{}, fmt.Errorf("open database: %w", err)
 	}
-	return nil
+
+	db.SetConnMaxLifetime(config.ConnMaxLifetime)
+	db.SetConnMaxIdleTime(config.ConnMaxIdleTime)
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	if config.MaxIdleConns != -1 {
+		db.SetMaxIdleConns(config.MaxIdleConns)
+	}
+
+	if err := pingDB(ctx, db, config.PingTimeout); err != nil {
+		if err := db.Close(); err != nil {
+			log.Errorwe(ctx, "failed to close db connection", err)
+		}
+		return Connector{}, fmt.Errorf("ping db: %w", err)
+	}
+
+	return Connector{db: db}, nil
+}
+
+func (c Connector) QueryContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error) {
+	return c.db.QueryxContext(ctx, query, args...)
+}
+
+func (c Connector) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sqlx.Row {
+	return c.db.QueryRowxContext(ctx, query, args...)
+}
+
+func (c Connector) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	return c.db.ExecContext(ctx, query, args...)
+}
+
+func (c Connector) Close() error {
+	return c.db.Close()
+}
+
+func (c Connector) PingContext(ctx context.Context) error {
+	return c.db.PingContext(ctx)
+}
+
+func (c Connector) SqlxDB() *sqlx.DB {
+	return c.db
+}
+
+func (c Connector) SqlDB() *sql.DB {
+	return c.db.DB
+}
+
+func (c Connector) Stats() sql.DBStats {
+	return c.db.DB.Stats()
+}
+
+func pingDB(ctx context.Context, db *sqlx.DB, timeout time.Duration) error {
+	if timeout > 0 {
+		var ctxCancel context.CancelFunc
+		ctx, ctxCancel = context.WithTimeout(ctx, timeout)
+		defer ctxCancel()
+	}
+	return db.PingContext(ctx)
 }
